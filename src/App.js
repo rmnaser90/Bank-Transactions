cca1d96a2c6fa3386067659183a2065255c6cd50
rmnaser90 <rmnaser90@gmail.com>
2020-12-30 22:46:08 +0200

    trying to fix ajax request error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home';
 import Operation from './components/Operation';
-import ApiManager from './utilities/ApiManager'
+import axios from 'axios'
+
+class ApiManager {
+    constructor() {
+        this.url = '/'
+    }
+    async getTransactions() {
+        return await axios.get(`${this.url}/transactions`)
+    }
+    async postTransaction(transaction) {
+        return await axios.post(`${this.url}/transaction`, transaction)
+    }
+    async deleteTransaction(transactionId) {
+        return await axios.put(`${this.url}/transaction/${transactionId}`)
+    }
+}
 const apiManager = new ApiManager()
 
 class App extends Component {
